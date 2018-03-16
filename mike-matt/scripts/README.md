8f946465424d0ce1aec2fd9c9856bfa03993fe24
Matt Harding <matt@wherethehellismatt.com>
2018-03-16 11:51:13 -0700

    finished remaining steps

---
+++ mike-matt/scripts/README.md
@@ -0,0 +1,28 @@
+# 04 Templating
+
+**Author**: Your Name Goes Here
+**Version**: 1.0.0 (increment the patch/fix version number up if you make more commits past your first submission)
+
+## Overview
+Online blog that dynamically displays articles based on user input.
+
+## Getting Started
+1. Fork the repo.
+2. Download files.
+3. Ensure all files are linked.
+
+## Architecture
+HTML5, CSS, JS, jQuery, handlebar templating
+
+## Change Log
+1. Added handlebars.js script to bottom of index.html 9:30am
+2. Created our handlebar template script in index.html 9:45am
+3. Created our handlebar function in article.js 10:00am
+4. Created a helper function to display days ago 10:30am
+5. Refactored our functions with arrows 11:10am
+6. Resolved (with Shannon's help) an ascii character bug when sending code through slack 11:30am
+7. resolved filter population bug by removing template class from our handlebar template 11:45am
+
+## Credits and Collaborations
+Michael Castor and Matt Harding
+with help from Shannon and Nicholas
--- mike-matt/scripts/articleView.js
+++ mike-matt/scripts/articleView.js
@@ -2,13 +2,13 @@
 
 let articleView = {};
 
-// TODO: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
+// DONE: Where possible, refactor methods into arrow functions, including the document.ready() method at the bottom.
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
 // Arrow functions create their own "this." context and constructor functions do not.
 //****If it does not rely on the contextual "this" from the constructor function.
 
-articleView.populateFilters = function() {
+articleView.populateFilters = () => {
   $('article').each(function() {
     if (!$(this).hasClass('template')) {
       let val = $(this).find('address a').text();
@@ -27,7 +27,7 @@ articleView.populateFilters = function() {
   });
 };
 
-articleView.handleAuthorFilter = function() {
+articleView.handleAuthorFilter = () => {
   $('#author-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
@@ -40,7 +40,7 @@ articleView.handleAuthorFilter = function() {
   });
 };
 
-articleView.handleCategoryFilter = function() {
+articleView.handleCategoryFilter = () => {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
       $('article').hide();
@@ -53,7 +53,7 @@ articleView.handleCategoryFilter = function() {
   });
 };
 
-articleView.handleMainNav = function() {
+articleView.handleMainNav = () => {
   $('.main-nav').on('click', '.tab', function() {
     $('.tab-content').hide();
     $(`#${$(this).data('content')}`).fadeIn();
@@ -62,7 +62,7 @@ articleView.handleMainNav = function() {
   $('.main-nav .tab:first').click();
 };
 
-articleView.setTeasers = function() {
+articleView.setTeasers = () => {
   $('.article-body *:nth-of-type(n+2)').hide();
   $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
@@ -79,7 +79,7 @@ articleView.setTeasers = function() {
   });
 };
 
-$(document).ready(function() {
+$(document).ready(() => {
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
